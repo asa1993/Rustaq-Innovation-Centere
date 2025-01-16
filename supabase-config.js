d084f3f3083bd013f56c49cfccca47d3568871fc
asa1993 <u097580@gmail.com>
2025-01-16 10:41:17 +0400

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+// Supabase Configuration
+const SUPABASE_URL = 'https://qguwiwaltreecrbquoti.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFndXdpd2FsdHJlZWNyYnF1b3RpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4NTQwNjEsImV4cCI6MjA1MDQzMDA2MX0.fFsZnqY-Prz8NYL55dhSY7AIl-FAlApvoZnwSbn8iMc';
